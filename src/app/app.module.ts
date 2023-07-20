d6c8848b4af5ca5a722d566fbf1b4444c2516ee8
IDGS-901-19002027 <19002027@alumnos.utleon.edu.mx>
2023-07-20 11:53:43 -0600

    Proyecto V1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app.routing.module';
+import { AuthComponent } from './login/auth/auth.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SignupComponent } from './login/signup/signup.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    MenuComponent,
+    SignupComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AuthComponent } from './login/auth/auth.component';
+import { SignupComponent } from './login/signup/signup.component';
+import { ContactComponent } from './contact/contact.component';
+import { UsComponent } from './us/us.component';
+
+const routes: Routes = [
+    { path: '', redirectTo: '/auth', pathMatch: 'full' },
+    { path: 'auth', component: AuthComponent },
+    { path: 'signup', component: SignupComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: 'us', component: UsComponent }
+
+];
+
+@NgModule({
+    imports:[RouterModule.forRoot(routes)],
+    exports:[RouterModule]
+})
+
+export class AppRoutingModule{
+    
+}
+
